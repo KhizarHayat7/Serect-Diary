7ad3104dcb090edae8c70cfda4ee2c551eb0ef46
Khizar Hayat <itsdmmeswee@gmail.com>
2026-07-21 13:53:25 +0500

    Update logout.js

--- Public/js/logout.js
+++ Public/js/logout.js
@@ -3,7 +3,7 @@ console.log("heeel")
 
 logoutBtn.addEventListener("onclick", async () => {
 
-    await fetch("http://localhost:8080/api/auth/logout", {
+    await fetch("/api/auth/logout", {
         method: "POST",
         credentials: "include"
     });
